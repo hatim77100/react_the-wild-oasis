f0245380b1d5849498b54b9402ea7e39ec924fe2
hatim77100 <najoua-hatim@hotmail.fr>
2024-02-10 15:55:47 +0100

    🔧 add pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,33 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-const StyledApp = styled.div`
-  background-color: orange;
-  padding: 20px;
-`;
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-
-      <StyledApp>
-        <Row type="vertical">
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
-            <div>
-              <Heading as="h2">Forms</Heading>
-              <Button>Check in</Button>
-              <Button variation="secondary" size="medium">
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row type="vertical">
-            <Heading as="h3">tree</Heading>
-            <form>
-              <Input
-                type="number"
-                placeholder="Number of guests"
-                Number
-                of
-                guests
-              />
-
-              <Input
-                type="number"
-                placeholder="Number of guests"
-                Number
-                of
-                guests
-              />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="users" element={<Users />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="account" element={<Account />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
 export default App;
